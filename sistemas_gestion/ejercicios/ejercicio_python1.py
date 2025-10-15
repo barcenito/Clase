75088e85bfcedbe8bbf6b6e872fe30a5a3c6c7ab
barcenito <carlos.barce.villalon@gmail.com>
2025-10-15 14:17:17 +0200

    Update ejercicio_python1.py

--- sistemas_gestion/ejercicios/ejercicio_python1.py
+++ sistemas_gestion/ejercicios/ejercicio_python1.py
@@ -23,21 +23,36 @@ class Producto():
 
 class ProductosDAO():
     def __init__(self):
-        self.productos = []
         self.path = './base_datos.txt'
+        self.file = None
 
-    def agregar_producto(self, producto):
-        self.productos.append(producto)
-
-    def cargar_productos(self):
+    def conectar(self):
         if os.path.exists(self.path):
-            with open(self.path, 'r') as file:
-                return 0
+            self.file = open(self.path, 'rw+')
+    
+    def cerrar_conexion(self):
+        if self.file:
+            self.file.close()
+
+    def guardar_producto(self, producto):
+        if self.file:
+            self.file.write(f"{producto.get_nombre()}-{producto.get_precio()}-{producto.get_cantidad()}\n")
+            self.file.flush()
+    
+    def buscar_producto(self, nombre):
+        for line in self.file:
+            datos = line.strip().split('-')
+            if datos[0] == nombre:
+                return Producto(datos[0], float(datos[1]), int(datos[2]))
 
     def listar_productos(self):
         return self.productos
 
-
+    def add_productos(self, productos):
+        self.conectar()
+        for producto in productos:
+            self.guardar_producto(producto)
+            
 file_path = './base_datos.txt'
 if os.path.exists(file_path):
     with open(file_path, 'r') as file:
