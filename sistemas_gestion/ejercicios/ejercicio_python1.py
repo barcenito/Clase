2605d0c0e7653ab7448af2cdf6980f219bfba6e1
barcenito <carlos.barce.villalon@gmail.com>
2025-10-15 14:03:04 +0200

    Update ejercicio_python1.py

--- sistemas_gestion/ejercicios/ejercicio_python1.py
+++ sistemas_gestion/ejercicios/ejercicio_python1.py
@@ -1,20 +1,30 @@
 import os
 
 class Producto():
-    def __init__(self, _id, nombre, precio):
-        self.id = _id
+    def __init__(self, nombre, precio, cantidad):
         self.nombre = nombre
         self.precio = precio
+        self.cantidad = cantidad
+    #getters
     def get_nombre(self):
         return self.nombre
     def get_precio(self):
         return self.precio
+    def get_cantidad(self):
+        return self.cantidad
+    #setters
+    def set_nombre(self, nombre):
+        self.nombre = nombre
+    def set_precio(self, precio):
+        self.precio = precio
+    def set_cantidad(self, cantidad):
+        self.cantidad = cantidad
+    
 
 class ProductosDAO():
     def __init__(self):
         self.productos = []
         self.path = './base_datos.txt'
-        self.cargar_productos()
 
     def agregar_producto(self, producto):
         self.productos.append(producto)
@@ -22,7 +32,7 @@ class ProductosDAO():
     def cargar_productos(self):
         if os.path.exists(self.path):
             with open(self.path, 'r') as file:
-                return
+                return 0
 
     def listar_productos(self):
         return self.productos
